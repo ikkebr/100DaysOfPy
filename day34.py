2b0d1c23e38a09a1917b72946a63cdd200d77e91
Henrique Pereira <ikkibr@gmail.com>
2021-09-26 14:21:08 -0600

    Day 34: better example

--- day34.py
+++ day34.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-# We can use finally to clean up after a try ... except block.
-try:
-    in_file = open("input.txt")
-    number = int( in_file.read(10) )
-    print(f"I have read the number {number}")
-except ValueError:
-    print("I couldn't read a number.")
-except Exception as e:
-    print("Something went wrong! " + e)
-finally:
-    print("This will get executed regardless if we hit an exception or not.")
+# Using a finally on a try .. except block will change the flow of execution of a function
+# before the return is executed, the finally block is triggered and takes precedence
+def divide(x, n):
+    try:
+        number = x/n
+        print(f"The result is {number}")
+        return number
+    except ZeroDivisionError:
+        print("I cannot divide by 0!")
+        return -1
+    finally:
+        print("This will get returned regardless if we hit exceptions or not.")
+        # uncomment the next line and the function will always return -2
+        # return -2
 
-    # let's make sure the file gets closed.
-    in_file.close()
+
+divide(1, 0)
