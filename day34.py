60ecdc9454b127712a983d14e168d85845580e26
Henrique Pereira <ikkibr@gmail.com>
2021-09-26 14:21:55 -0600

    Day 34: better wording

--- day34.py
+++ day34.py
@@ -11,7 +11,7 @@ def divide(x, n):
         print("I cannot divide by 0!")
         return -1
     finally:
-        print("This will get returned regardless if we hit exceptions or not.")
+        print("This will get executed regardless if we hit exceptions or not.")
         # uncomment the next line and the function will always return -2
         # return -2
 
