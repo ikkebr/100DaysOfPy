5a2d6aa99214667f8a8c255ac621f76db2837401
Henrique Pereira <ikkibr@gmail.com>
2021-09-29 08:12:35 -0600

    Day 37: better comments.

--- day36.py
+++ day36.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+
+# Creates a decorator that manipulates the return value of a function
 def to_upper(func):
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs).upper()
     return wrapper
 
+# We use @ to place the decorator
 @to_upper
 def say(message = 'Hi', times = 1):
     return message * times
 
 
+# the result of the say function is now in uppercase
 print(say("Henrique ", 3)) # HENRIQUE HENRIQUE HENRIQUE
--- day37.py
+++ day37.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
+# Let's create a Box class that has a name and some storage
 class Box(object):
     def __init__(self, name='MyBox'):
         self.name = name
         self.storage = []
     
 my_box = Box("My First Box")
+
+# we can then insert things in the storage
 my_box.storage.append("abc")
 my_box.storage.append("cde")
 
+# we can also show what is in storage
 print(my_box.storage) # prints ['abc', 'cde']
